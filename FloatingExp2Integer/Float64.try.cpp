039519903143bc80e809777c6482dfd9a84afb83
Robustic <juhas.malinen@gmail.com>
2025-02-28 08:43:46 +0200

    More tight limits for random numbers

--- FloatingExp2Integer/Float64.try.cpp
+++ FloatingExp2Integer/Float64.try.cpp
@@ -204,7 +204,7 @@ void loop(int n, double results[], std::int64_t time[]) {
 void InitializeRandomNumbers(std::vector<double>& vec) {
     std::mt19937 rng;
     rng.seed(seed_val);
-    std::uniform_real_distribution<float> unif(std::log2(1e-15), std::log2(1e-10));
+    std::uniform_real_distribution<float> unif(std::log2(1e-11), std::log2(1e-10));
     for (unsigned int i = 0; i < vec.size(); i++) {
         float a_random_float = unif(rng);
         vec[i] = (double)std::exp2f(a_random_float);
