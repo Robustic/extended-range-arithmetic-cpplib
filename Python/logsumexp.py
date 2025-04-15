8b89c9007d7dd676973187529424b73ad8a04753
Malinen <juha.malinen@sweco.fi>
2025-04-15 22:15:34 +0300

    Python file updated

--- Python/logsumexp.py
+++ Python/logsumexp.py
@@ -15,11 +15,12 @@ def ln_to_log2(ln_value):
     return ln_value / math.log(2)
 
 # Calculate log-sum-exp using torch
-def calculate_log_sum_exp(doubles):
+def calculate_log_sum_exp(doubles, repeat_count):
     tensor = torch.tensor(doubles, dtype=torch.float64)
     
     start_time = time.perf_counter()
-    log_sum_exp = torch.logsumexp(tensor, dim=0)
+    for _ in range(repeat_count):
+        log_sum_exp = torch.logsumexp(tensor, dim=0)
     end_time = time.perf_counter()
     
     execution_time_nanoseconds = (end_time - start_time) * 1_000_000_000
@@ -34,23 +35,23 @@ def log2_to_ln(log2_value):
     return log2_value * math.log(2)
 
 # Read doubles from binary file
-doubles_log2 = read_binary_file('data.bin')
+doubles_log2 = read_binary_file('log2range_-100_to_0.bin')
 doubles = [log2_to_ln(value) for value in doubles_log2]
 
 print("torch.get_num_threads():", torch.get_num_threads())
 
 for _ in range(1):
-    result = calculate_log_sum_exp(doubles[:1000])
-    result = calculate_log_sum_exp(doubles[:3000])
-    result = calculate_log_sum_exp(doubles[:10000])
-    result = calculate_log_sum_exp(doubles[:30000])
-    result = calculate_log_sum_exp(doubles[:100000])
-    result = calculate_log_sum_exp(doubles[:300000])
-    result = calculate_log_sum_exp(doubles[:1000000])
-    result = calculate_log_sum_exp(doubles[:3000000])
-    result = calculate_log_sum_exp(doubles[:10000000])
-    result = calculate_log_sum_exp(doubles[:30000000])
-    result = calculate_log_sum_exp(doubles)
+    result = calculate_log_sum_exp(doubles[:1000], 10000)
+    result = calculate_log_sum_exp(doubles[:3000], 10000)
+    result = calculate_log_sum_exp(doubles[:10000], 10000)
+    result = calculate_log_sum_exp(doubles[:30000], 3000)
+    result = calculate_log_sum_exp(doubles[:100000], 1000)
+    result = calculate_log_sum_exp(doubles[:300000], 300)
+    result = calculate_log_sum_exp(doubles[:1000000], 100)
+    result = calculate_log_sum_exp(doubles[:3000000], 30)
+    result = calculate_log_sum_exp(doubles[:10000000], 10)
+    result = calculate_log_sum_exp(doubles[:30000000], 3)
+    result = calculate_log_sum_exp(doubles, 1)
 
 # C:\msys64\home\FloatingExp2Integer\Python>python logsumexp.py
 # torch.get_num_threads(): 8
